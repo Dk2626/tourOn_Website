b97fa809fb694512e238b1e72c639ef2bfa264b0
Dk2626 <dineshkumardssd@gmail.com>
2022-04-07 12:00:51 -0700

    restrict text in whatsapp no

--- src/Tour Categories/Reusable components/Checkout.js
+++ src/Tour Categories/Reusable components/Checkout.js
@@ -1,9 +1,9 @@
-import React, { useEffect } from "react"
-import "./Checkout.css"
-import "./Destination.css"
-import { Ripple } from "react-spinners-css"
-import { firedb } from "../../firebase"
-import { isAuthenticated } from "../../Login components/auth"
+import React, { useEffect } from 'react';
+import './Checkout.css';
+import './Destination.css';
+import { Ripple } from 'react-spinners-css';
+import { firedb } from '../../firebase';
+import { isAuthenticated } from '../../Login components/auth';
 
 const Checkout = ({
   setName,
@@ -36,47 +36,52 @@ const Checkout = ({
   // console.log("dfsd", number)
   // console.log("dfsd", typeof(number))
   // console.log("dfsd", name !== "" && budget >= 50000 && number !== "" && number !== undefined && (number !== undefined && number.length == 10))
-  
 
-  const handleKeyDown = e => {
-    if (e.key === " ") {
+  const handleKeyDown = (e) => {
+    if (e.key === ' ') {
+      e.preventDefault();
+    }
+  };
+
+  const handleKeyPress = (e) => {
+    if (!/[0-9]/.test(e.key)) {
       e.preventDefault();
     }
   };
 
   return (
-    <div className="destination-container">
+    <div className='destination-container'>
       {/* <img className="checkout-image" src={imgSrc} alt="" /> */}
-      <div className="destination-questions">
-        <div className="que1">
+      <div className='destination-questions'>
+        <div className='que1'>
           <h6>Enter your Name</h6>
           <input
-            type="text"
+            type='text'
             onChange={(e) => {
-              setName(e.target.value)
+              setName(e.target.value);
             }}
             value={name}
           />
         </div>
-        <div className="que2">
+        <div className='que2'>
           <h6>Your Budget / Person</h6>
-          {tourCategory === "LuxuryTour" ? (
+          {tourCategory === 'LuxuryTour' ? (
             <>
-              {tourType === "Domestic" ? (
+              {tourType === 'Domestic' ? (
                 <input
-                  type="number"
-                  placeholder="Minimum Rs.50000"
+                  type='number'
+                  placeholder='Minimum Rs.50000'
                   onChange={(e) => {
-                    setBudget(e.target.value)
+                    setBudget(e.target.value);
                   }}
                   value={budget}
                 />
               ) : (
                 <input
-                  type="number"
-                  placeholder="Minimum Rs.75000"
+                  type='number'
+                  placeholder='Minimum Rs.75000'
                   onChange={(e) => {
-                    setBudget(e.target.value)
+                    setBudget(e.target.value);
                   }}
                   value={budget}
                 />
@@ -84,33 +89,33 @@ const Checkout = ({
             </>
           ) : (
             <>
-              {tourType === "Domestic" ? (
+              {tourType === 'Domestic' ? (
                 <input
-                  type="number"
-                  placeholder="Minimum Rs.10000"
+                  type='number'
+                  placeholder='Minimum Rs.10000'
                   onChange={(e) => {
-                    setBudget(e.target.value)
+                    setBudget(e.target.value);
                   }}
                   value={budget}
                 />
               ) : (
                 <>
-                  {tourCategory === "RoadTrip" ||
-                  tourCategory === "WildlifeTour" ? (
+                  {tourCategory === 'RoadTrip' ||
+                  tourCategory === 'WildlifeTour' ? (
                     <input
-                      type="number"
-                      placeholder="Minimum Rs.20000"
+                      type='number'
+                      placeholder='Minimum Rs.20000'
                       onChange={(e) => {
-                        setBudget(e.target.value)
+                        setBudget(e.target.value);
                       }}
                       value={budget}
                     />
                   ) : (
                     <input
-                      type="number"
-                      placeholder="Minimum Rs.40000"
+                      type='number'
+                      placeholder='Minimum Rs.40000'
                       onChange={(e) => {
-                        setBudget(e.target.value)
+                        setBudget(e.target.value);
                       }}
                       value={budget}
                     />
@@ -120,73 +125,85 @@ const Checkout = ({
             </>
           )}
         </div>
-        <div className="que3">
+        <div className='que3'>
           <h6>Whatsapp Number</h6>
           <input
-            type="text"
+            type='text'
             onChange={(e) => {
-              setNumber(e.target.value)
+              setNumber(e.target.value);
             }}
             value={number}
-            maxLength="10"
+            maxLength='10'
             onKeyDown={handleKeyDown}
+            onKeyPress={handleKeyPress}
+            // onKeyPress={(event) => {
+            //   if (!/[0-9]/.test(event.key)) {
+            //     event.preventDefault();
+            //   }
+            // }}
           />
         </div>
       </div>
-      <div className="navigation_btn">
+      <div className='navigation_btn'>
         {subLoaded ? (
-          <Ripple color="coral" />
+          <Ripple color='coral' />
         ) : (
           <>
-            <button className="previous-button" onClick={() => prevStep()}>
+            <button className='previous-button' onClick={() => prevStep()}>
               Previous
             </button>
-            {tourCategory === "LuxuryTour" ? (
+            {tourCategory === 'LuxuryTour' ? (
               <>
-                {tourType === "Domestic" ? (
+                {tourType === 'Domestic' ? (
                   <>
-                    {name !== "" && budget >= 50000 && number !== "" && number !== undefined && (number !== undefined && number.length == 10) ? (
+                    {name !== '' &&
+                    budget >= 50000 &&
+                    number !== '' &&
+                    number !== undefined &&
+                    number !== undefined &&
+                    number.length == 10 ? (
                       <button
-                        className="submit-button"
+                        className='submit-button'
                         onClick={() => {
-                          submitData()
-                        }}
-                      >
+                          submitData();
+                        }}>
                         Submit
                       </button>
                     ) : (
                       <button
                         disabled
-                        className="submit-button"
-                        style={{ color: "#c1c1c1" }}
+                        className='submit-button'
+                        style={{ color: '#c1c1c1' }}
                         onClick={() => {
-                          submitData()
-                        }}
-                      >
+                          submitData();
+                        }}>
                         Submit
                       </button>
                     )}
                   </>
                 ) : (
                   <>
-                    {name !== "" && budget >= 75000 && number !== "" && number !== undefined && (number !== undefined && number.length == 10) ? (
+                    {name !== '' &&
+                    budget >= 75000 &&
+                    number !== '' &&
+                    number !== undefined &&
+                    number !== undefined &&
+                    number.length == 10 ? (
                       <button
-                        className="submit-button"
+                        className='submit-button'
                         onClick={() => {
-                          submitData()
-                        }}
-                      >
+                          submitData();
+                        }}>
                         Submit
                       </button>
                     ) : (
                       <button
                         disabled
-                        className="submit-button"
-                        style={{ color: "#c1c1c1" }}
+                        className='submit-button'
+                        style={{ color: '#c1c1c1' }}
                         onClick={() => {
-                          submitData()
-                        }}
-                      >
+                          submitData();
+                        }}>
                         Submit
                       </button>
                     )}
@@ -195,77 +212,86 @@ const Checkout = ({
               </>
             ) : (
               <>
-                {tourType === "Domestic" ? (
+                {tourType === 'Domestic' ? (
                   <>
-                    {name !== "" && budget >= 10000 && number !== "" && number !== undefined && (number !== undefined && number.length == 10) ? (
+                    {name !== '' &&
+                    budget >= 10000 &&
+                    number !== '' &&
+                    number !== undefined &&
+                    number !== undefined &&
+                    number.length == 10 ? (
                       <button
-                        className="submit-button"
+                        className='submit-button'
                         onClick={() => {
-                          submitData()
-                        }}
-                      >
+                          submitData();
+                        }}>
                         Submit
                       </button>
                     ) : (
                       <button
                         disabled
-                        className="submit-button"
-                        style={{ color: "#c1c1c1" }}
+                        className='submit-button'
+                        style={{ color: '#c1c1c1' }}
                         onClick={() => {
-                          submitData()
-                        }}
-                      >
+                          submitData();
+                        }}>
                         Submit
                       </button>
                     )}
                   </>
                 ) : (
                   <>
-                    {tourCategory === "RoadTrip" ||
-                    tourCategory === "WildlifeTour" ? (
+                    {tourCategory === 'RoadTrip' ||
+                    tourCategory === 'WildlifeTour' ? (
                       <>
-                        {name !== "" && budget >= 20000 && number !== "" && number !== undefined && (number !== undefined && number.length == 10) ? (
+                        {name !== '' &&
+                        budget >= 20000 &&
+                        number !== '' &&
+                        number !== undefined &&
+                        number !== undefined &&
+                        number.length == 10 ? (
                           <button
-                            className="submit-button"
+                            className='submit-button'
                             onClick={() => {
-                              submitData()
-                            }}
-                          >
+                              submitData();
+                            }}>
                             Submit
                           </button>
                         ) : (
                           <button
                             disabled
-                            className="submit-button"
-                            style={{ color: "#c1c1c1" }}
+                            className='submit-button'
+                            style={{ color: '#c1c1c1' }}
                             onClick={() => {
-                              submitData()
-                            }}
-                          >
+                              submitData();
+                            }}>
                             Submit
                           </button>
                         )}
                       </>
                     ) : (
                       <>
-                        {name !== "" && budget >= 40000 && number !== "" && number !== undefined && (number !== undefined && number.length == 10) ? (
+                        {name !== '' &&
+                        budget >= 40000 &&
+                        number !== '' &&
+                        number !== undefined &&
+                        number !== undefined &&
+                        number.length == 10 ? (
                           <button
-                            className="submit-button"
+                            className='submit-button'
                             onClick={() => {
-                              submitData()
-                            }}
-                          >
+                              submitData();
+                            }}>
                             Submit
                           </button>
                         ) : (
                           <button
                             disabled
-                            className="submit-button"
-                            style={{ color: "#c1c1c1" }}
+                            className='submit-button'
+                            style={{ color: '#c1c1c1' }}
                             onClick={() => {
-                              submitData()
-                            }}
-                          >
+                              submitData();
+                            }}>
                             Submit
                           </button>
                         )}
@@ -279,7 +305,7 @@ const Checkout = ({
         )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Checkout
+export default Checkout;
